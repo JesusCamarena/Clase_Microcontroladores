f27c220604dd68d7eedbbf4709845ca72fd3abbe
Jesus Camarena <jesus.garzacmn@uanl.edu.mx>
2023-03-01 21:02:05 -0600

    Frimware P0

--- Ejemplos_Atmega328P/00_HolaMundo/Firmware/Ej2HolaMundo/main.c
+++ Ejemplos_Atmega328P/00_HolaMundo/Firmware/Ej2HolaMundo/main.c
@@ -15,10 +15,8 @@
 #include <avr/io.h> //I/O del AVR
 
 //-----------------Main---------------------//
-
 int main(void)//se ejecuta una vez (setup)
 {           
-
 //DDRX =  Registro de 8 bits donde se declara si es una entrada o salida
 //0b =  dato en binario
 			//76543210 numero de los bits del registro
@@ -28,13 +26,13 @@ int main(void)//se ejecuta una vez (setup)
 //0b =  dato en binario
 	PORTB = 0b00100000; //PORTB5 se enciende
 	
-	        //76543210
+            //76543210
 	DDRC  = 0b00000100; //PORTC2 se declara como salida
 	PORTC = 0b00000100; //PORTC2 se enciende
 //-----------------while---------------------//	
     while (1) //bucle infinito (loop)
     {
 		
-    }
-}
+    }//Fin_while
+}//Fin_main
 
