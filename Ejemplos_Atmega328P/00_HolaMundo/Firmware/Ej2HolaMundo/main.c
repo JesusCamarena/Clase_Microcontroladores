70ae1de0c57361fb13c14de3469f852c30a86ee8
Jesus Camarena <jesus.garzacmn@uanl.edu.mx>
2023-03-01 20:59:48 -0600

    Firmware P0

--- Ejemplos_Atmega328P/00_HolaMundo/Firmware/Ej2HolaMundo/main.c
+++ Ejemplos_Atmega328P/00_HolaMundo/Firmware/Ej2HolaMundo/main.c
@@ -4,24 +4,34 @@
  * Created: 09/02/2023 10:52:05 a. m.
  * Author : jdgar
  */ 
+
 //comentario corto
 //atmega328p
 /*
-	Comentario largo
+	Comentario largooooooo
 */
+
 //--Bibliotecas
 #include <avr/io.h> //I/O del AVR
 
+//-----------------Main---------------------//
 
+int main(void)//se ejecuta una vez (setup)
+{           
 
-int main(void)//setup 
-{           //76543210
-	DDRB  = 0b00100000; //Declara como salida
-	PORTB = 0x00;// Enciende
-	        //76543210
-	DDRC  = 0b00000100;
-	PORTC = 0x00;
+//DDRX =  Registro de 8 bits donde se declara si es una entrada o salida
+//0b =  dato en binario
+			//76543210 numero de los bits del registro
+	DDRB  = 0b00100000; //PORTB5 se declara como salida
 	
+//PORTX =  Registro de 8 bits donde se declara si esta encendido o apagado
+//0b =  dato en binario
+	PORTB = 0b00100000; //PORTB5 se enciende
+	
+	        //76543210
+	DDRC  = 0b00000100; //PORTC2 se declara como salida
+	PORTC = 0b00000100; //PORTC2 se enciende
+//-----------------while---------------------//	
     while (1) //bucle infinito (loop)
     {
 		
